9a33c3f094cd07fa7f26db928a2709f1ad0ebed8
Nick <najarvis2016@gmail.com>
2018-10-31 15:31:11 -0400

    Basic string input working

--- ZeldaBot.lua
+++ ZeldaBot.lua
@@ -1,14 +1,28 @@
 up = true
 pressed = false
 
+frame_index = 0
+--[[
+    Possible values in the search string
+    a = a button
+    b = b button
+    u = up button
+    d = down button
+    l = left button
+    r = right button
+    s = start
+    e = select
+]]
+search_string = ""
+
 function get_health(normalized)
     hearts = memory.readbyte(tonumber("0x66F"))
     partial = memory.readbyte(tonumber("0x670"))
 
+    -- Low Nibble = how many hearts are filled. High Nibble = Number of heart containers - 1
     filled_hearts = bit.rshift(bit.lshift(hearts, 4), 4) - 32
     num_heart_containers = bit.rshift(hearts, 4) + 1
 
-    partial_filled = 0
     if partial == 0 then
         partial_filled = 0
     elseif partial < tonumber("0x7F") then
@@ -21,6 +35,7 @@ function get_health(normalized)
 
     normalized_health = total_health / num_heart_containers
 
+    -- If normalized isn't specified it is passed in as 'nil', which evaluates to false
     if normalized then
         return normalized_health
     else
@@ -28,33 +43,56 @@ function get_health(normalized)
     end
 end
 
-while true do 
+function generate_random_string(length)
+    search_string = ""
+    valid_options = "abudlr"
+    for i=1,length do
+        r_index = math.random(1, string.len(valid_options))
+        search_string = search_string .. string.rep(string.sub(valid_options, r_index, r_index), math.random(1, 50))
+    end
+    print(search_string)
+end
+
+generate_random_string(100)
+
+while true do
+
     -- Print input from player 1
-    print(joypad.getdown(1))
 
     xPos = memory.readbyte(tonumber("0x70"))
     yPos = memory.readbyte(tonumber("0x84"))
-
-    -- print(get_health())
+    
+    -- gui.text(0, 20, "(" .. xPos .. ", " .. yPos .. ")")
+    
     dead = get_health() == 0
 
     if dead then
         pressed = not pressed
     end
 
-    if yPos < 100 and up then
-        up = false
-    end
-    if yPos > 150 and not up then
-        up = true
+    curr = string.sub(search_string, frame_index, frame_index)
+    gui.text(0, 20, "Selected Button: " .. curr)
+    
+    my_input = {
+        a = curr == 'a',
+        b = curr == 'b',
+        left = curr == 'l',
+        right = curr == 'r',
+        up = curr == 'u',
+        down = curr == 'd',
+        start = curr == 's' or pressed,
+        select = curr == 'e'
+    }
+
+    frame_index = frame_index + 1
+    if frame_index > string.len(search_string) then
+        frame_index = 1
+        generate_random_string(100)
     end
 
-    my_input = {up=up, down=not up, start=pressed}
-    print(my_input)
+    -- print(my_input)
     joypad.write(1, my_input)
 
-    gui.text(0, 20, "(" .. xPos .. ", " .. yPos .. ")")
-    -- gui.text(0, 50, yPos)
-    -- joypad.write(1, {left=true})
+    
     emu.frameadvance()
 end
